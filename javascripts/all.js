c4a82b1e2a0e1ec08e8c1acc37b9dcab058f0c65
cjeria <chjeria@gmail.com>
2014-10-26 20:53:48 -0700

    initial commit

---
+++ javascripts/all.js
@@ -0,0 +1,2 @@
+//=
+$(document).ready(function(){$("ul li.portrait").hover(function(){$("ul#main").addClass("hover")},function(){$("ul#main").removeClass("hover")}),$("a[title]").tooltips(),$('*[data-tooltip="2"]').addClass("portrait-toolip")});
